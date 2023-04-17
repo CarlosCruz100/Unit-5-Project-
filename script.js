8baad1067b18020b71935ba6b0a56871544eb2df
CARLOS CRUZ <121175341+CarlosCruz100@users.noreply.github.com>
2023-04-17 12:45:50 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let input1 = document.querySelector(".input1")
+let input2 = document.querySelector(".input2")
+let results = document.querySelector(".results")
+
+input1.onclick = function(){
+  let input1.value = 1-6;
+} 
+input2.onclick = function(){
+  let input2.value = "Messi";
+}
